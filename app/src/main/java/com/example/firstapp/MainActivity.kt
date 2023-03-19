04d76b4f9e70064fa30f3016365de17d340e17da
Davy ROBERT <davy.robert@bluesoft-group.com>
2023-03-19 20:37:14 +0100

    add event on click button connect

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.firstapp    // Chemin où est situé notre MainActivity.kt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     /* La classe MainActivity hérite de la classe AppCompatActivity qui nous permet d'avoir toutes les fonctionnalités, les attributs qui ns permettent d'intéragir avec l'interface web (XML). */
@@ -15,5 +17,15 @@ class MainActivity : AppCompatActivity() {
 */
         setContentView(R.layout.auth_page_fb_relative_layout)
 
+        val connect = findViewById<Button>(R.id.connect)
+        connect.setOnClickListener(View.OnClickListener { view: View ->
+            println("Hello, je suis le bouton connect")
+        })
+/*        connect.setOnClickListener{
+            println("Hello, je suis le bouton connect ${it.id} !!")
+        }*/
     }
 }
+
+/*
+View.OnClickListener est une lambda expression, c est une classe anonyme et on peut ne pas l écrire.*/
