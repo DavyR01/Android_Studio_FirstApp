c32bfe6ba9c96c22ab7530e7946b558def2cbeac
Davy ROBERT <davy.robert@bluesoft-group.com>
2023-03-20 12:09:14 +0100

    add listPosts from code Kotlin in HomeActivity

--- app/src/main/java/com/example/firstapp/HomeActivity.kt
+++ app/src/main/java/com/example/firstapp/HomeActivity.kt
@@ -2,7 +2,9 @@ package com.example.firstapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.TextView
 
 class HomeActivity : AppCompatActivity() {
@@ -11,17 +13,20 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-/*
-        val tvHello = findViewById<TextView>(R.id.tvHello)
-*/
+/*        val tvHello = findViewById<TextView>(R.id.tvHello)*/
 
         // 1. Récupérer l'email envoyé par l'activityMain
         val email = intent.getStringExtra("email")
 
+
+        val listPosts = findViewById<ListView>(R.id.listPosts)
+        val postsArray = listOf("Post 1", "Post 2", "Post 3", "Post 4", "Post 5")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, postsArray)
+        listPosts.adapter = adapter
+
+
         // 2. Afficher l'email dans le tvHello
-/*
-        tvHello.text = "Bienvenue : $email"
-*/
+/*        tvHello.text = "Bienvenue : $email"*/
 
     }
 }
